a52d47c09e2ba0677397919214a9cbf19ba1e879
Egor <Egor060705@yandex.ru>
2025-12-23 00:18:47 +0700

    Лабораторна 3

--- cmd/life-and-health-insurance/main.go
+++ cmd/life-and-health-insurance/main.go
@@ -1,6 +1,33 @@
 package main
-import "fmt"
+
+import (
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+
+func CreatePolicy(w http.ResponseWriter, r *http.Request) {
+    policy := mux.Vars(r)
+    w.Write([]byte("Оформлен новый страховой полис " + policy["id"]))
+}
+
+func SubmitInsuranceClaim(w http.ResponseWriter, r *http.Request) {
+    claim := mux.Vars(r)
+    w.Write([]byte("Подано заявление о страховом случае " + claim["id"]))
+}
+
+func CalculateCompensation(w http.ResponseWriter, r *http.Request) {
+    calc := mux.Vars(r)
+    w.Write([]byte("Рассчитана сумма выплаты для случая " + calc["id"]))
+}
 
 func main() {
-    fmt.Println("Привет, мир!")
+    r := mux.NewRouter()
+
+    r.HandleFunc("/policies", CreatePolicy).Methods("POST")
+    r.HandleFunc("/claims", SubmitInsuranceClaim).Methods("POST")
+    r.HandleFunc("/calculate/{id}", CalculateCompensation).Methods("GET")
+
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
